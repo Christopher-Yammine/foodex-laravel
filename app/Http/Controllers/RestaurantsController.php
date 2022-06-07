c62176aba016b44a5027fd24d2519dd15cbcac0e
ChristopherYammine <chrisyammine98@gmail.com>
2022-06-07 18:52:44 +0300

    created a get restaurants api

--- app/Http/Controllers/RestaurantsController.php
+++ app/Http/Controllers/RestaurantsController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Restaurant;
 
-class RestaurantsController extends Controller
-{
-    //
+class RestaurantsController extends Controller{
+
+    public function getAllRestos(){
+        $restos=Restaurant::join('restaurant_types','restaurant_types.id_restaurant_type','=','restaurants.restaurant_type')
+        ->join('locations','locations.id_location','=','restaurants.location_id')
+        ->get('restaurants.*','restaurant_types.type','locations.city_name');
+        return response()->json([
+            "status" => "Success",
+            "restos" => $restos
+        ], 200);
+    }
 }
+
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+use App\Http\Controllers\RestaurantsController;
+
+Route::get('/AllRestos', [RestaurantsController::class, 'getAllRestos'])->name("AllRestos");
